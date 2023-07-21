fea6e1edefc67ae2fc5116473a1b178a62e3ec95
Ashwin <aaccapadi@findhelp.com>
2023-07-20 23:55:48 -0500

    push

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -45,8 +45,29 @@ const ExpenseForm = () => {
         // })
     }
 
+    // const inputChangeHandler = (identifier, value) => {
+    //     if (identifier === 'title') {
+    //         setEnteredTitle(value)
+    //     } else if (identifier === 'date') {
+    //         setEnteredDate(value)
+    //     } else {
+    //         setEnteredAmount(value)
+    //     }
+    // }
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+        console.log(expenseData)
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
